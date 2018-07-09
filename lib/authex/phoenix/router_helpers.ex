f95c3ab3005c997f2bce95050b0048613b9ca73f
Dan Schultzer <dan@dreamconception.com>
2018-07-08 17:19:21 -0700

    Refactor RouterHelpers

--- lib/authex/phoenix/router_helpers.ex
+++ lib/authex/phoenix/router_helpers.ex
@@ -12,13 +12,17 @@ defmodule Authex.Phoenix.RouterHelpers do
 
   @spec after_sign_out_path(Conn.t()) :: binary()
   def after_sign_out_path(conn) do
-    config = Plug.fetch_config(conn)
-    Config.get(config, :after_sign_out_path, helpers(conn).authex_session_path(conn, :new))
+    route(conn, :after_sign_out_path, helpers(conn).authex_session_path(conn, :new))
   end
 
   @spec after_sign_in_path(Conn.t()) :: binary()
   def after_sign_in_path(conn) do
-    config = Plug.fetch_config(conn)
-    Config.get(config, :after_sign_in_path, "/")
+    route(conn, :after_sign_in_path, "/")
+  end
+
+  defp route(conn, path, default) do
+    conn
+    |> Plug.fetch_config()
+    |> Config.get(path, default)
   end
 end
