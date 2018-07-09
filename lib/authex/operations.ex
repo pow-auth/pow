f6c142d28ffaa2ed6b859de96d5fd2b38f4420b4
Dan Schultzer <dan@dreamconception.com>
2018-07-09 08:35:17 -0700

    Pass config to Users

--- lib/authex/operations.ex
+++ lib/authex/operations.ex
@@ -6,27 +6,27 @@ defmodule Authex.Operations do
 
   @spec authenticate(Config.t(), map()) :: {:ok, map()} | {:error, term()} | no_return
   def authenticate(config, params) do
-    module(config).authenticate(params)
+    module(config).authenticate(config, params)
   end
 
   @spec changeset(Config.t(), map()) :: map()
   def changeset(config, user_or_params) do
-    module(config).changeset(user_or_params)
+    module(config).changeset(config, user_or_params)
   end
 
   @spec create(Config.t(), map()) :: {:ok, map()} | {:error, map()} | no_return
   def create(config, params) do
-    module(config).create(params)
+    module(config).create(config, params)
   end
 
   @spec update(Config.t(), map(), map()) :: {:ok, map()} | {:error, map()} | no_return
   def update(config, user, params) do
-    module(config).update(user, params)
+    module(config).update(config, user, params)
   end
 
   @spec delete(Config.t(), map()) :: {:ok, map()} | {:error, map()} | no_return
   def delete(config, user) do
-    module(config).delete(user)
+    module(config).delete(config, user)
   end
 
   defp module(config) do
--- test/support/user_mock.ex
+++ test/support/user_mock.ex
@@ -2,18 +2,18 @@ defmodule Authex.Test.UserMock do
   @valid_params %{"email" => "test@example.com", "password" => "secret"}
   @invalid_params %{"email" => "test@example.com"}
 
-  def authenticate(@valid_params), do: {:ok, %{id: 1}}
-  def authenticate(@invalid_params), do: {:error, :invalid_password}
-  def authenticate(_params), do: {:error, :not_found}
+  def authenticate(_config, @valid_params), do: {:ok, %{id: 1}}
+  def authenticate(_config, @invalid_params), do: {:error, :invalid_password}
+  def authenticate(_config, _params), do: {:error, :not_found}
 
-  def changeset(params), do: params
+  def changeset(_config, params), do: params
 
-  def create(@valid_params), do: {:ok, %{id: 1}}
-  def create(params), do: {:error, params}
+  def create(_config, @valid_params), do: {:ok, %{id: 1}}
+  def create(_config, params), do: {:error, params}
 
-  def update(%{id: 1}, @valid_params), do: {:ok, %{id: 1, updated: true}}
-  def update(_user, params), do: {:error, params}
+  def update(_config, %{id: 1}, @valid_params), do: {:ok, %{id: 1, updated: true}}
+  def update(_config, _user, params), do: {:error, params}
 
-  def delete(%{id: 1}), do: {:ok, %{id: 1}}
-  def delete(user), do: {:error, user}
+  def delete(_config, %{id: 1}), do: {:ok, %{id: 1}}
+  def delete(_config, user), do: {:error, user}
 end
