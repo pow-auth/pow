3cdda4e93e46beb8cbe6b7abf517ee67aac72ba2
Dan Schultzer <dan@dreamconception.com>
2018-07-18 10:32:45 -0700

    Add mix task for extension templates

--- lib/mix/tasks/phoenix/authex.phoenix.gen.templates.ex
+++ lib/mix/tasks/phoenix/authex.phoenix.gen.templates.ex
@@ -8,7 +8,7 @@ defmodule Mix.Tasks.Authex.Phoenix.Gen.Templates do
   """
   use Mix.Task
 
-  alias Mix.{Authex.Utils, Generator}
+  alias Mix.Authex.{Phoenix, Utils}
 
   @switches [context_app: :string]
   @default_opts []
@@ -29,43 +29,17 @@ defmodule Mix.Tasks.Authex.Phoenix.Gen.Templates do
   ]
 
   defp create_template_files(config) do
-    structure    = Mix.Authex.Phoenix.Utils.parse_structure(config)
+    structure    = Phoenix.Utils.parse_structure(config)
     context_base = structure[:context_base]
     web_module   = structure[:web_module]
     web_prefix   = structure[:web_prefix]
 
     Enum.each @templates, fn {name, actions} ->
-      create_view_file(name, web_module, web_prefix)
-      create_templates(name, web_prefix, actions)
+      Phoenix.Utils.create_view_file(Authex, name, web_module, web_prefix)
+      Phoenix.Utils.create_templates(Authex, name, web_prefix, actions)
     end
 
     %{context_base: context_base, web_module: web_module}
-  end
-
-  defp create_view_file(name, web_mod, web_prefix) do
-    path        = Path.join([web_prefix, "views", "authex", "#{name}_view.ex"])
-
-    content     = """
-    defmodule #{inspect web_mod}.Authex.#{Macro.camelize(name)}View do
-      use #{inspect web_mod}, :view
-    end
-    """
-
-    Generator.create_file(path, content)
-  end
-
-  defp create_templates(name, web_prefix, actions) do
-    path = Path.join([web_prefix, "templates", "authex", name])
-    template_module = Module.concat([Authex.Phoenix, "#{Macro.camelize(name)}Template"])
-
-    actions
-    |> Enum.map(&String.to_existing_atom/1)
-    |> Enum.each(fn action ->
-      content   = template_module.html(action)
-      file_path = Path.join(path, "#{action}.html.eex")
-
-      Generator.create_file(file_path, content)
-    end)
   end
 
   defp print_shell_instructions(%{context_base: mod, web_module: web_mod}) do
--- lib/mix/tasks/phoenix/utils.ex
+++ lib/mix/tasks/phoenix/utils.ex
@@ -1,4 +1,9 @@
 defmodule Mix.Authex.Phoenix.Utils do
+  @moduledoc """
+  Utilities module for mix phoenix tasks.
+  """
+  alias Mix.Generator
+
   @spec parse_structure(map()) :: map()
   def parse_structure(config) do
     context_app  = Map.get(config, :context_app, Mix.Authex.Utils.context_app())
@@ -21,5 +26,32 @@ defmodule Mix.Authex.Phoenix.Utils do
       true  -> Module.concat(["#{base}Web"])
       false -> Module.concat([base])
     end
+  end
+
+  @spec create_view_file(atom(), binary(), atom(), binary()) :: :ok
+  def create_view_file(module, name, web_mod, web_prefix) do
+    path    = Path.join([web_prefix, "views", Macro.underscore(module), "#{name}_view.ex"])
+    content = """
+    defmodule #{inspect web_mod}.#{inspect module}.#{Macro.camelize(name)}View do
+      use #{inspect web_mod}, :view
+    end
+    """
+
+    Generator.create_file(path, content)
+  end
+
+  @spec create_templates(atom(), binary(), binary(), [binary()]) :: :ok
+  def create_templates(module, name, web_prefix, actions) do
+    path            = Path.join([web_prefix, "templates", Macro.underscore(module), name])
+    template_module = Module.concat([module, Phoenix, "#{Macro.camelize(name)}Template"])
+
+    actions
+    |> Enum.map(&String.to_existing_atom/1)
+    |> Enum.each(fn action ->
+      content   = template_module.html(action)
+      file_path = Path.join(path, "#{action}.html.eex")
+
+      Generator.create_file(file_path, content)
+    end)
   end
 end
