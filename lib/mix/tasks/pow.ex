7deea8861857c3af09055db47bbea03eba8be034
Dan Schultzer <dan@dreamconception.com>
2018-07-21 17:03:01 -0700

    Add base mix task

---
+++ lib/mix/tasks/pow.ex
@@ -0,0 +1,26 @@
+defmodule Mix.Tasks.Pow do
+  use Mix.Task
+
+  @shortdoc "Prints Pow help information"
+
+  @moduledoc """
+  Prints Pow tasks and their information.
+      mix pow
+  """
+
+  @doc false
+  def run(args) do
+    case args do
+      [] -> general()
+      _ -> Mix.raise "Invalid arguments, expected: mix pow"
+    end
+  end
+
+  defp general() do
+    Application.ensure_all_started(:pow)
+    Mix.shell.info "Pow v#{Application.spec(:pow, :vsn)}"
+    Mix.shell.info "A user authentication solution for Plug and Phoenix apps."
+    Mix.shell.info "\nAvailable tasks:\n"
+    Mix.Tasks.Help.run(["--search", "pow."])
+  end
+end
