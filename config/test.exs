27d4e77d85729da7cc5d55ab173bda77cf835738
Dan Schultzer <dan@dreamconception.com>
2018-07-28 01:48:21 -0700

    Move app env config to test.exs

--- config/test.exs
+++ config/test.exs
@@ -13,3 +13,21 @@ config :pow, Pow.Test.Ecto.Repo,
 config :pbkdf2_elixir, rounds: 1
 
 config :mnesia, dir: 'tmp/mnesia'
+
+for extension <- [PowEmailConfirmation, PowResetPassword] do
+  context_module = Module.concat([extension, Test])
+  web_module = Module.concat([extension, TestWeb])
+
+  config context_module, Module.concat([web_module, Phoenix.Endpoint]),
+    render_errors: [view: Module.concat([web_module, Phoenix.ErrorView]), accepts: ~w(html json)],
+    secret_key_base: String.duplicate("abcdefghijklmnopqrstuvxyz0123456789", 2)
+
+  config context_module, :pow,
+    user: Module.concat([context_module, Users.User]),
+    repo: Module.concat([context_module, RepoMock]),
+    cache_store_backend: Pow.Test.EtsCacheMock,
+    mailer_backend: Pow.Test.Phoenix.MailerMock,
+    messages_backend: Module.concat([web_module, Phoenix.Messages]),
+    extensions: [extension],
+    controller_callbacks: Pow.Extension.Phoenix.ControllerCallbacks
+end
--- test/support/extensions/mock.ex
+++ test/support/extensions/mock.ex
@@ -4,17 +4,6 @@ defmodule Pow.Test.ExtensionMocks do
     context_module = __CALLER__.module
     web_module     = String.to_atom("#{context_module}Web")
 
-    Code.eval_quoted(quote do
-      Application.put_env(unquote(context_module), :pow,
-        user: Module.concat([unquote(context_module), Users.User]),
-        repo: Module.concat([unquote(context_module), RepoMock]),
-        cache_store_backend: Pow.Test.EtsCacheMock,
-        mailer_backend: Pow.Test.Phoenix.MailerMock,
-        messages_backend: Module.concat([unquote(web_module), Phoenix.Messages]),
-        extensions: unquote(opts)[:extensions] || [],
-        controller_callbacks: Pow.Extension.Phoenix.ControllerCallbacks)
-    end)
-
     module = Module.concat([context_module, Users.User])
     quoted = quote do
       use Ecto.Schema
@@ -58,17 +47,10 @@ defmodule Pow.Test.ExtensionMocks do
     end
     Module.create(module, quoted, Macro.Env.location(__ENV__))
 
-    endpoint_module = Module.concat([web_module, Phoenix.Endpoint])
-    error_view_module = Module.concat([web_module, Phoenix.ErrorView])
-    endpoint_config = [
-      render_errors: [view: error_view_module, accepts: ~w(html json)],
-      secret_key_base: String.duplicate("abcdefghijklmnopqrstuvxyz0123456789", 2)]
-    Application.put_env(:pow, endpoint_module, endpoint_config)
+    module = Module.concat([web_module, Phoenix.Endpoint])
 
     quoted = quote do
-      use Phoenix.Endpoint, otp_app: :pow
-
-      def init(_key, _config), do: {:ok, unquote(endpoint_config)}
+      use Phoenix.Endpoint, otp_app: unquote(context_module)
 
       plug Plug.RequestId
       plug Plug.Logger
@@ -90,7 +72,7 @@ defmodule Pow.Test.ExtensionMocks do
 
       plug unquote(web_module).Phoenix.Router
     end
-    Module.create(endpoint_module, quoted, Macro.Env.location(__ENV__))
+    Module.create(module, quoted, Macro.Env.location(__ENV__))
 
     module = Module.concat([web_module, Phoenix.ConnCase])
     quoted = quote do
