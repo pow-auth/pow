f09f2c285db234119117096c66c21bce4a90f358
Dan Schultzer <dan@dreamconception.com>
2018-07-28 22:01:57 -0700

    Move Ecto test setup to test_helper

--- test/support/ecto/test_case.ex
+++ test/support/ecto/test_case.ex
@@ -5,24 +5,10 @@ defmodule Pow.Test.Ecto.TestCase do
   alias Ecto.Adapters.SQL.Sandbox
   alias Pow.Test.Ecto.Repo
 
-  setup_all do
-    create_test_database()
-    {:ok, _pid} = Repo.start_link()
-    Sandbox.mode(Repo, :manual)
-
-    :ok
-  end
-
   setup do
     :ok = Sandbox.checkout(Repo)
     Sandbox.mode(Repo, {:shared, self()})
 
     :ok
-  end
-
-  defp create_test_database do
-    Mix.Task.run "ecto.drop", ~w(--quiet -r Pow.Test.Ecto.Repo)
-    Mix.Task.run "ecto.create", ~w(--quiet -r Pow.Test.Ecto.Repo)
-    Mix.Task.run "ecto.migrate", ~w(--quiet -r Pow.Test.Ecto.Repo)
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,3 +2,10 @@ Mix.shell(Mix.Shell.Process)
 Logger.configure(level: :warn)
 
 ExUnit.start()
+
+Mix.Task.run "ecto.drop", ~w(--quiet -r Pow.Test.Ecto.Repo)
+Mix.Task.run "ecto.create", ~w(--quiet -r Pow.Test.Ecto.Repo)
+Mix.Task.run "ecto.migrate", ~w(--quiet -r Pow.Test.Ecto.Repo)
+
+{:ok, _pid} = Pow.Test.Ecto.Repo.start_link()
+Ecto.Adapters.SQL.Sandbox.mode(Pow.Test.Ecto.Repo, :manual)
