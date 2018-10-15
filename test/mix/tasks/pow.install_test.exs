2fc772873f469bcbd3d2313d9172d60717456d0c
Dan Schultzer <dan@dreamconception.com>
2018-10-15 10:14:16 -0700

    Add a better umbrella error for mix pow install task

--- test/mix/tasks/pow.install_test.exs
+++ test/mix/tasks/pow.install_test.exs
@@ -21,4 +21,24 @@ defmodule Mix.Tasks.Pow.InstallTest do
       assert File.ls!("lib/pow/users") == ["user.ex"]
     end)
   end
+
+  test "raises error in umbrella app" do
+    File.cd!(@tmp_path, fn ->
+      File.write!("mix.exs", """
+      defmodule Umbrella.MixProject do
+        use Mix.Project
+
+        def project do
+          [apps_path: "apps"]
+        end
+      end
+      """)
+
+      Mix.Project.in_project(:umbrella, ".", fn _ ->
+        assert_raise Mix.Error, "mix pow.install can't be used in umbrella apps. Run mix pow.ecto.install in your ecto app directory, and mix pow.phoenix.install in your phoenix app directory.", fn ->
+          Install.run([])
+        end
+      end)
+    end)
+  end
 end
