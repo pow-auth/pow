198400e76ee11baad842ffdd397adcb181ffcbd0
Dan Schultzer <dan@dreamconception.com>
2019-01-19 09:15:44 -0800

    Add user and conn assigns to mail

--- lib/pow/phoenix/mailer/mail.ex
+++ lib/pow/phoenix/mailer/mail.ex
@@ -17,16 +17,17 @@ defmodule Pow.Phoenix.Mailer.Mail do
   """
   @spec new(Conn.t(), map(), {module(), atom()}, Keyword.t()) :: t()
   def new(conn, user, {view_module, template}, assigns) do
-    config     = Plug.fetch_config(conn)
-    web_module = Config.get(config, :web_mailer_module)
+    config       = Plug.fetch_config(conn)
+    web_module   = Config.get(config, :web_mailer_module)
+    view_assigns = Keyword.merge([conn: conn, user: user], assigns)
 
     view_module = Pow.Phoenix.ViewHelpers.build_view_module(view_module, web_module)
 
     subject = subject(conn, view_module, template)
-    text    = render(conn, view_module, "#{template}.text", assigns)
+    text    = render(conn, view_module, "#{template}.text", view_assigns)
     html    =
       conn
-      |> render(view_module, "#{template}.html", assigns)
+      |> render(view_module, "#{template}.html", view_assigns)
       |> Phoenix.Template.HTML.encode_to_iodata!()
       |> IO.iodata_to_binary()
 
