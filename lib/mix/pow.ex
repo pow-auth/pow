756ba80de5180ac769ed36ccd6b11f047634c993
Dan Schultzer <dan@dreamconception.com>
2019-03-07 07:38:36 -0800

    Raise error with helper for invalid schema args

--- lib/mix/pow.ex
+++ lib/mix/pow.ex
@@ -98,8 +98,38 @@ defmodule Mix.Pow do
   """
   @spec schema_options_from_args([binary()]) :: map()
   def schema_options_from_args(_opts \\ [])
-  def schema_options_from_args([schema_name, schema_plural | _rest]), do: %{schema_name: schema_name, schema_plural: schema_plural}
+  def schema_options_from_args([schema, plural | _rest]), do: %{schema_name: schema, schema_plural: plural}
   def schema_options_from_args(_any), do: %{schema_name: "Users.User", schema_plural: "users"}
+
+  @doc false
+  @spec validate_schema_args!([binary()], binary()) :: map()
+  def validate_schema_args!([schema, plural | _rest] = args, task) do
+    cond do
+      not schema_valid?(schema) ->
+        raise_invalid_schema_args_error("Expected the schema argument, #{inspect schema}, to be a valid module name", task)
+      not plural_valid?(plural) ->
+        raise_invalid_schema_args_error("Expected the plural argument, #{inspect plural}, to be all lowercase using snake_case convention", task)
+      true ->
+        schema_options_from_args(args)
+    end
+  end
+  def validate_schema_args!([_schema | _rest], task) do
+    raise_invalid_schema_args_error("Invalid arguments", task)
+  end
+  def validate_schema_args!([], _task), do: schema_options_from_args()
+
+  defp schema_valid?(schema), do: schema =~ ~r/^[A-Z]\w*(\.[A-Z]\w*)*$/
+
+  defp plural_valid?(plural), do: plural =~ ~r/^[a-z\_]*$/
+
+  defp raise_invalid_schema_args_error(msg, task) do
+    Mix.raise("""
+    #{msg}
+
+    mix #{task} accepts both a module name and the plural of the resource:
+        mix #{task} Users.User users
+    """)
+  end
 
   @doc """
   Fetches context app for the project.
