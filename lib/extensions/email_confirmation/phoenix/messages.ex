3d84a6ba9f9bb0d5c2fad00331e8884adfbe63d9
Dan Schultzer <dan@dreamconception.com>
2019-03-15 09:08:07 -0700

    Add messages documentation

--- lib/extensions/email_confirmation/phoenix/messages.ex
+++ lib/extensions/email_confirmation/phoenix/messages.ex
@@ -1,8 +1,25 @@
 defmodule PowEmailConfirmation.Phoenix.Messages do
   @moduledoc false
+
+  @doc """
+  Flash message to show when email has been confirmed.
+  """
   def email_has_been_confirmed(_conn), do: "The email address has been confirmed."
+
+  @doc """
+  Flash message to show when email couldn't be confirmed.
+  """
   def email_confirmation_failed(_conn), do: "The email address couldn't be confirmed."
+
+  @doc """
+  Flash message to show when user is signs in or registers but e-mail is yet
+  to be confirmed.
+  """
   def email_confirmation_required(_conn), do: "You'll need to confirm your e-mail before you can sign in. An e-mail confirmation link has been sent to you."
 
+  @doc """
+  Flash message to show when user updates their e-mail and requires
+  confirmation.
+  """
   def email_confirmation_required_for_update(_conn), do: "You'll need to confirm the e-mail before it's updated. An e-mail confirmation link has been sent to you."
 end
