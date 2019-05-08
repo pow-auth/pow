c956010b01bfcc9d084e5dd9c360f914ffc58dbc
Dan Schultzer <dan@dreamconception.com>
2019-05-08 08:26:23 -0700

    Separate conn helpers and process store

--- test/support/conn_helpers.ex
+++ test/support/conn_helpers.ex
@@ -1,31 +1,3 @@
-defmodule Plug.ProcessStore do
-  @moduledoc false
-  @behaviour Plug.Session.Store
-
-  def init(_opts) do
-    nil
-  end
-
-  def get(_conn, sid, nil) do
-    {sid, Process.get({:session, sid}) || %{}}
-  end
-
-  def delete(_conn, sid, nil) do
-    Process.delete({:session, sid})
-    :ok
-  end
-
-  def put(conn, nil, data, nil) do
-    sid = Base.encode64(:crypto.strong_rand_bytes(96))
-    put(conn, sid, data, nil)
-  end
-
-  def put(_conn, sid, data, nil) do
-    Process.put({:session, sid}, data)
-    sid
-  end
-end
-
 defmodule Pow.Test.ConnHelpers do
   @moduledoc false
   alias Plug.{Conn, ProcessStore, Session, Test}
---
+++ test/support/process_store.ex
@@ -0,0 +1,29 @@
+defmodule Plug.ProcessStore do
+  @moduledoc """
+  Used with `Plug.Conn` sessions in tests. See `Pow.Test.ConnHelpers` for more.
+  """
+  @behaviour Plug.Session.Store
+
+  def init(_opts) do
+    nil
+  end
+
+  def get(_conn, sid, nil) do
+    {sid, Process.get({:session, sid}) || %{}}
+  end
+
+  def delete(_conn, sid, nil) do
+    Process.delete({:session, sid})
+    :ok
+  end
+
+  def put(conn, nil, data, nil) do
+    sid = Base.encode64(:crypto.strong_rand_bytes(96))
+    put(conn, sid, data, nil)
+  end
+
+  def put(_conn, sid, data, nil) do
+    Process.put({:session, sid}, data)
+    sid
+  end
+end
