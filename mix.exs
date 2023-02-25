e898091e17fb605a9791c2d920d8a205e0f2dbae
Dan Schultzer <1254724+danschultzer@users.noreply.github.com>
2023-02-24 18:05:55 -0800

    Update phoenix dependency requirement

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Pow.MixProject do
   defp deps do
     [
       {:ecto, "~> 2.2 or ~> 3.0"},
-      {:phoenix, ">= 1.3.0 and < 1.7.0"},
+      {:phoenix, ">= 1.3.0 and < 1.8.0"},
       {:phoenix_html, ">= 2.0.0 and < 4.0.0"},
       {:plug, ">= 1.5.0 and < 2.0.0", optional: true},
 
