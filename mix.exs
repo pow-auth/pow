df147c7c4fb3c245e938b2c16cedc6837ee28d7b
Dan Schultzer <1254724+danschultzer@users.noreply.github.com>
2023-02-27 07:14:32 -0800

    Handle view changes in phoenix 1.7

--- mix.exs
+++ mix.exs
@@ -36,6 +36,7 @@ defmodule Pow.MixProject do
       {:ecto, "~> 2.2 or ~> 3.0"},
       {:phoenix, ">= 1.3.0 and < 1.8.0"},
       {:phoenix_html, ">= 2.0.0 and < 4.0.0"},
+      {:phoenix_view, "~> 2.0"},
       {:plug, ">= 1.5.0 and < 2.0.0", optional: true},
 
       {:phoenix_ecto, "~> 4.3", only: [:dev, :test]},
--- test/pow/phoenix/controllers/controller_test.exs
+++ test/pow/phoenix/controllers/controller_test.exs
@@ -9,11 +9,12 @@ defmodule Pow.Phoenix.ControllerTest do
       conn =
         conn
         |> Conn.put_private(:pow_config, web_module: Phoenix, user: User)
-        |> Conn.put_private(:phoenix_view, SessionView)
+        |> Conn.put_private(:phoenix_view, %{_: SessionView})
         |> Conn.put_private(:phoenix_router, Router)
         |> Conn.put_private(:phoenix_action, :new)
         |> Conn.put_private(:phoenix_endpoint, Endpoint)
-        |> Conn.put_private(:phoenix_layout, {LayoutView, :app})
+        |> Conn.put_private(:phoenix_format, :html)
+        |> Conn.put_private(:phoenix_layout, %{html: {LayoutView, :app}})
         |> Conn.assign(:action, "#")
 
       conn = ViewHelpers.layout(conn)
