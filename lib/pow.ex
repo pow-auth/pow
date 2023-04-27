ed8965d1257c8608cc5b037d00dc879a7282eb63
Dan Schultzer <1254724+danschultzer@users.noreply.github.com>
2023-04-27 14:33:08 -0700

    Ensure dependency is loaded when fetching version

--- lib/pow.ex
+++ lib/pow.ex
@@ -3,17 +3,16 @@ defmodule Pow do
 
   @doc """
   Checks for version requirement in dependencies.
+
+  The dependency will be loaded into the environment first.
   """
   @spec dependency_vsn_match?(atom(), binary()) :: boolean()
   def dependency_vsn_match?(dep, req) do
-    case :application.get_key(dep, :vsn) do
-      {:ok, actual} ->
-        actual
-        |> List.to_string()
-        |> Version.match?(req)
+    Application.load(dep)
 
-      _any ->
-        false
+    case Application.spec(dep, :vsn) do
+      nil -> false
+      vsn -> Version.match?(List.to_string(vsn), req)
     end
   end
 end
