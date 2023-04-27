d51b60547dc684bd7abe9f7c20edd55307cad29d
Dan Schultzer <1254724+danschultzer@users.noreply.github.com>
2023-04-27 12:45:56 -0700

    Load deps at compile-time

--- lib/pow.ex
+++ lib/pow.ex
@@ -1,19 +1,20 @@
 defmodule Pow do
+  @deps Enum.map(Mix.Dep.cached(), &{&1.app, &1.status})
+
   @moduledoc false
 
   @doc """
   Checks for version requirement in dependencies.
+
+  The dependencies are loaded from the cache at compile-time.
   """
   @spec dependency_vsn_match?(atom(), binary()) :: boolean()
   def dependency_vsn_match?(dep, req) do
-    case :application.get_key(dep, :vsn) do
-      {:ok, actual} ->
-        actual
-        |> List.to_string()
-        |> Version.match?(req)
-
-      _any ->
-        false
+    @deps
+    |> Keyword.get(dep, nil)
+    |> case do
+      {:ok, actual} -> Version.match?(actual, req)
+      _error -> false
     end
   end
 end
